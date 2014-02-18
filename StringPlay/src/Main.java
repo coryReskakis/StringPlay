b61be4857b94265deef53b3a6da56573edc9bcc4
coryReskakis <cory.reskakis@merck.com>
2014-02-17 22:31:55 -0500

    Exiting message and proper breaks in switch block

--- StringPlay/src/Main.java
+++ StringPlay/src/Main.java
@@ -13,6 +13,7 @@ public class Main {
 			System.out.println("** 1) Reverse a string.                  **");
 			System.out.println("** 2) Exit.                              **");
 			System.out.println("*******************************************");
+			System.out.println("Selection:");
 			input = br.readLine();
 			choice = Integer.parseInt(input);
 			switch(choice) {
@@ -21,7 +22,9 @@ public class Main {
 				System.out.println("Please enter a string:");
 				String userInput = br.readLine();
 				//To be implemented... reverse a string algo
+				break;
 			case 2:
+				System.out.println("Exiting...");
 				System.exit(0);
 				}
 			}
